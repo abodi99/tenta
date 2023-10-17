0eabea2c019cf0f90aab9fcbed6161dffd300274
abodi99 <78203043+abodi99@users.noreply.github.com>
2023-10-18 00:51:46 +0200

    Update test_base.py

--- tenta/test/test_base.py
+++ tenta/test/test_base.py
@@ -3,36 +3,57 @@ import pytest
 
 BASE_URL = "http://localhost:80/api/v3"
 
-@pytest.fixture
-def pet_id():
-    response = requests.post(f"{BASE_URL}/pet", json={
-        "id": 123,
-        "name": "TestPet",
-        "status": "available"
-    })
-    return response.json()['id']
 
-def test_add_new_pet():
-    response = requests.post(f"{BASE_URL}/pet", json={
-        "id": 123,
-        "name": "TestPet",
-        "status": "available"
-    })
-    assert response.status_code == 200
+def check_if_pet_exists(pet_id):
+    # Send a GET request to check if the pet with the given ID exists.
+    response = requests.get(f"{BASE_URL}/pet/{pet_id}")
+
+    # Check if the response status code is 200 (Pet exists) or 404 (Pet not found).
+    if response.status_code == 200:
+        return True
+    elif response.status_code == 404:
+        return False
+    else:
+        # Return None for other status codes (e.g., 400 - Invalid ID supplied).
+        return False
+
+pet_data = [
+    {"id": 1, "name": "Pet1", "status": "available"},
+    {"id": 10, "name": "Pet2", "status": "pending"},
+    {"id": 300, "name": "Pet3", "status": "sold"},
+    {"id": 250, "name": "Pet4", "status": "available"},
+]
+@pytest.mark.parametrize("pet_info", pet_data)
+def test_add_new_pet(pet_info):
+    # Check if the pet already exists.
+    pet_id = pet_info["id"]
+
+    pet_exists = check_if_pet_exists(pet_id)
+
+    # If the pet doesn't exist, add a new pet and verify it returns a 200 status code.
+    assert(pet_exists==False)
+    if not pet_exists:
+        response = requests.post(f"{BASE_URL}/pet", json={
+            "id": pet_id,
+            "name": "TestPet",
+            "status": "available"
+        })
+        assert response.status_code == 200
 
 
-def test_delete_pet():
 
-    test_add_new_pet()
-    # Delete the pet with the given petId
-    response = requests.delete(f"{BASE_URL}/pet/123")
+@pytest.mark.parametrize("pet_id", [ 1, 10,100, 200, 300])
+def test_delete_pet(pet_id):
 
-    # Check if the response status code is 200, indicating a successful delete request.
-    assert response.status_code == 200
+    pet_exists = check_if_pet_exists(pet_id)
 
-    # Optionally, you can check if the pet has been deleted by trying to retrieve it.
-    verify_deleted_response = requests.get(f"{BASE_URL}/pet/123")
-    assert verify_deleted_response.status_code == 404
+    assert(pet_exists==True)
+    if pet_exists:
+        response = requests.delete(f"{BASE_URL}/pet/{pet_id}")
+        assert response.status_code == 200
+        verify_deleted_response = requests.get(f"{BASE_URL}/pet/{pet_id}")
+        assert verify_deleted_response.status_code == 404
+
 
 
 @pytest.mark.parametrize("status", ["available", "pending", "sold"])
@@ -46,4 +67,36 @@ def test_find_pets_by_status(status):
     pets = response.json()
     for pet in pets:
         assert pet["status"] == status
+
+
+def test_place_order():
+    order_statuses = ["placed", "approved", "delivered"]
+    
+    for order_status in order_statuses:
+        order_data = {
+            "id": 10,
+            "petId": 198772,
+            "quantity": 7,
+            "shipDate": "2023-10-17T17:49:08.712Z",
+            "status": order_status,
+            "complete": True
+        }
+
+        # Place an order with the current order status
+        response = requests.post(f"{BASE_URL}/store/order", json=order_data)
+
+        # Check if the response status code is 200, indicating a successful order placement.
+        assert response.status_code == 200
+
+        # Optionally, you can check the response content to further validate.
+        response_data = response.json()
+        assert response_data["status"] == order_status
+
+        # Ensure the shipDate is slightly different (updated by the server)
+        assert response_data["shipDate"] != order_data["shipDate"]
+
+
+
+
+
 
